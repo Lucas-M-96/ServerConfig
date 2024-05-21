39d31b45dfb14e4f409f6bf7d7c4fa1abc0ccf9e
Lucas-M-96 <lucasmainguet@hotmail.fr>
2024-05-21 12:54:00 +0200

    created sh script and changed test url

---
+++ nginx/nginx-config-script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+file_name="all_apps_https"
+
+source_path="~/github-actions-runner/_work/ServerConfig/ServerConfig/nginx/sites-available/${file_name}"
+available_path="/etc/nginx/sites-available/${file_name}"
+enabled_path="/etc/nginx/sites-enabled/${file_name}"
+
+if [ -f "$enabled_path" ]
+then
+    echo "NGINX Configuration file already exists, renewing file..."
+    rm "$enabled_path"
+else
+    echo "NGINX Configuration file doesn't exist, creating file..."
+fi
+
+cp "$source_path" > "$available_path"
+
+chcon unconfined_u:object_r:httpd_config_t:s0 "$available_path"
+chown root:root "$available_path"
+
+ln -s "$available_path" "$enabled_path"
+
+nginx -t
+systemctl reload nginx
